f90ab1717d11137d3716367bf96de3aa9ed71a66
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2022-09-03 13:50:49 +0300

    create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CIFAR10_Pruning_Batchnorm',
+      version='1.0',
+      description='Add pruning, train and test model',
+      author='mpushkareva',
+      url='https://github.com/mpushkareva/CIFAR10_Pruning_Batchnorm/'
+     )
