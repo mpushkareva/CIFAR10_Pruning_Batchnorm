7f356eb98233343201feee60ed2d51b583b87faa
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2022-09-03 14:06:24 +0300

    add find_packages

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
+
 
 setup(name='CIFAR10_Pruning_Batchnorm',
       version='1.0',
-      description='Add pruning, train and test model',
-      author='mpushkareva',
-      url='https://github.com/mpushkareva/CIFAR10_Pruning_Batchnorm/'
+      packages=find_packages(),
+      description='Add pruning, train and test model', 
+      install_requires=[
+        'numpy', 
+        'matplotlib',
+        'tqdm',
+        'torch',
+        'scikit-learn'
+    ]
      )
